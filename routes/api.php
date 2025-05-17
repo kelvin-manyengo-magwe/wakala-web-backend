21cbf1c4b6c56d7a6ce7d9886833a3be5ece15e7
kelvin magwe <kelvinmullar5@gmail.com>
2025-05-18 02:40:45 +0300

    Created the routes/api for Transaction controller with auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TransactionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//uses auth:sanctum middleware to protect routes with token based authenitication
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/transactions/sync', [TransactionController::class, 'sync']);
+    Route::get('/transactions', [TransactionController::class, 'index']);
+});
